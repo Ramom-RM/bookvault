5d065790e9ae62618dde75bc4fae8a6f780c095f
Ramom Ribeiro Martins <ramom.dev@outlook.com>
2026-03-07 08:54:16 -0300

    Versao Inicial

---
+++ controllers/livro.controller.php
@@ -0,0 +1,13 @@
+<?php
+
+$livro = $DB->query(
+
+    query: "select * from livros where id = :id",
+
+    class: Livro::class,
+
+    params: ['id' => $_GET['id']]
+
+)->fetch();
+
+view('livro', compact('livro'));
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require "functions.php";
+
+require "models/Livro.php";
+
+$config = require 'config.php';
+
+require "database.php";
+
+require "routes.php";
+
+?>
